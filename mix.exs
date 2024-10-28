f5b58b17c1509a26e96dfab7c7f8acc710b71ef2
Alex McLain <alex@alexmclain.com>
2024-10-27 22:45:27 -0700

    Update dialyzer config

--- mix.exs
+++ mix.exs
@@ -14,7 +14,6 @@ defmodule Tpm.MixProject do
       docs: docs(),
       test_coverage: [tool: ExCoveralls, test_task: "espec"],
       dialyzer: [
-        ignore_warnings: "dialyzer.ignore.exs",
         list_unused_filters: true,
         plt_file: {:no_warn, plt_file_path()},
       ],
