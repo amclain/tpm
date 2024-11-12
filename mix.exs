46dd526be14256182e66f0778062df814ef2d352
Alex McLain <alex@alexmclain.com>
2024-11-11 23:17:35 -0800

    Version 0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tpm.MixProject do
   def project do
     [
       app: :tpm,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
